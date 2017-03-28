c7b29d58fda8c5c33801ddd11b2d1115b8df419c
PatriciaCR <PatriciaCR@Patricias-MacBook-Pro-3.local>
2017-03-27 17:04:41 -0700

    final

--- Lab5/ViewController.swift
+++ Lab5/ViewController.swift
@@ -11,38 +11,66 @@ import CoreLocation
 import MapKit
 
 
-class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate{
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate, UITableViewDataSource, UITableViewDelegate{
 
-    @IBOutlet var myMap: MKMapView!
-    
-   
-    @IBOutlet var address: UITextField!
-    
-    
-    @IBOutlet var route: UITextView!
-    
+    @IBOutlet var autofillBt: UIButton!
+    @IBOutlet var directionsTableView: UITableView!
+    @IBOutlet var startingPt: UITextField!
+    @IBOutlet var destinationPt: UITextField!
     @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet var myMap: MKMapView!
+    @IBOutlet var locateS: UIButton!
+    @IBOutlet var locateD: UIButton!
+    @IBOutlet var getDirection: UIButton!
+    
     
     var myLocMgr = CLLocationManager()
     var myGeoCoder =  CLGeocoder()
-    var showPlaceMark : CLPlacemark?
+    var showPlaceMSt : CLPlacemark?
+    var showPlaceMDt : CLPlacemark?
     var toAddr : String?
+    var fromAddr : String?
     var currentTransportType = MKDirectionsTransportType.automobile
+    var steps : [MKRouteStep]!
+
+    
     
     // select transport type by default
-   // var currentTransportType = MKDirectionsTransportType.automobile
+    // var currentTransportType = MKDirectionsTransportType.automobile
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
+        locateS.layer.cornerRadius = 4
+        locateD.layer.cornerRadius = 4
+        getDirection.layer.cornerRadius = 4
+        autofillBt.layer.cornerRadius = 4
         segmentedControl.isHidden = true
+        
+        myMap.delegate = self
+        
         // Do any additional setup after loading the view, typically from a nib.
-      myLocMgr.requestWhenInUseAuthorization()
+        myLocMgr.requestWhenInUseAuthorization()
         let status = CLLocationManager.authorizationStatus()
         if status == CLAuthorizationStatus.authorizedWhenInUse
         {
             self.myMap.showsUserLocation = true
-        
         }
-        myMap.delegate = self
+        
+    }
+    
+    
+    
+    @IBAction func autofillCA(_ sender: Any) {
+        if (startingPt.text == "")
+        {
+            //startingPt.text = "latitude: "+"\(myMap.userLocation.coordinate.latitude)"+", longitude: "+"\(myMap.userLocation.coordinate.longitude)"+")"
+        }
+        
+        if(destinationPt.text == "")
+        {
+            //destinationPt.text = "("+"\(myMap.userLocation.coordinate.latitude)"+", "+"\(myMap.userLocation.coordinate.longitude)"+")"
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -50,24 +78,52 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         // Dispose of any resources that can be recreated.
     }
 
-    @IBOutlet var getLocation: UIButton!
-   
-    @IBOutlet var getDirection: UIButton!
-    
-    @IBAction func locate(_ sender: Any) {
-        self.toAddr = self.address!.text
+    @IBAction func locateStart(_ sender: Any) {
+        self.fromAddr = self.startingPt!.text
+        
+        myGeoCoder.geocodeAddressString(fromAddr!, completionHandler:
+            {
+                placemarks, error in
+                if error != nil{
+                    print(error!)
+                    let alert = UIAlertController(title: "No results found!", message: "Please enter a valid starting point", preferredStyle: UIAlertControllerStyle.alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                    return
+                }
+                //No error, got an array of placemarks - show the first
+                if placemarks != nil && placemarks!.count > 0
+                {
+                    let placemark = placemarks![0] as CLPlacemark
+                    self.showPlaceMSt = placemark
+                    //add annotation
+                    let annotation = MKPointAnnotation()
+                    annotation.title = placemark.name
+                    annotation.subtitle = self.fromAddr
+                    annotation.coordinate = placemark.location!.coordinate
+                    self.myMap.showAnnotations([annotation], animated: true)
+                }
+        })
+    }
+
+    @IBAction func locateDest(_ sender: Any) {
+        self.toAddr = self.destinationPt!.text
+        
         myGeoCoder.geocodeAddressString(toAddr!, completionHandler:
             {
                 placemarks, error in
                 if error != nil{
                     print(error!)
+                    let alert = UIAlertController(title: "No results found!", message: "Please enter a valid destination point", preferredStyle: UIAlertControllerStyle.alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
                     return
                 }
-                // no error, got an array of placemarks - show the first
+                //No error, got an array of placemarks - show the first
                 if placemarks != nil && placemarks!.count > 0
                 {
                     let placemark = placemarks![0] as CLPlacemark
-                    self.showPlaceMark = placemark
+                    self.showPlaceMDt = placemark
                     //add annotation
                     let annotation = MKPointAnnotation()
                     annotation.title = placemark.name
@@ -76,11 +132,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
                     self.myMap.showAnnotations([annotation], animated: true)
                 }
         })
-    }
 
-   
+    }
+    
     
     @IBAction func direction(_ sender: Any) {
+        directionsTableView.delegate = self
+        directionsTableView.dataSource = self
+        
         if segmentedControl.selectedSegmentIndex == 0 {
             currentTransportType = MKDirectionsTransportType.automobile
         } else {
@@ -90,15 +149,29 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         
         let dirRq = MKDirectionsRequest()
         let myTransportType = currentTransportType
-        segmentedControl.addTarget(self, action: "direction:", for: .valueChanged)
-        var myRoute : MKRoute?
-        var showRoute = ""
         
-        //set the source and destination of the route
-        dirRq.source = MKMapItem.forCurrentLocation()
-        dirRq.destination = MKMapItem(placemark: MKPlacemark(placemark: showPlaceMark!))
+        segmentedControl.addTarget(self, action: #selector(ViewController.direction(_:)), for: .valueChanged)
+        var myRoute : MKRoute?
+        
+        //set the source of the route
+        if(self.startingPt.text == ""){
+            dirRq.source = MKMapItem.forCurrentLocation()
+        }
+        else {
+            dirRq.source = MKMapItem(placemark: MKPlacemark(placemark: showPlaceMSt!))
+        }
+        
+        //set the destination of the route
+        if(self.destinationPt.text == ""){
+            dirRq.destination = MKMapItem.forCurrentLocation()
+        }
+        else {
+            dirRq.destination = MKMapItem(placemark: MKPlacemark(placemark: showPlaceMDt!))
+        }
+
         dirRq.transportType = myTransportType
         let myDirs = MKDirections(request: dirRq) as MKDirections
+        
         myDirs.calculate(completionHandler: {
             routeResponse, routeError in
             if routeError != nil
@@ -108,36 +181,45 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
             }
         else
             {
-            //get the first route
-            myRoute = routeResponse?.routes[0] as MKRoute!
+                //get the first route
+                myRoute = routeResponse?.routes[0] as MKRoute!
             
-            // remove an existing route line and add  a new one
-            self.myMap.removeOverlays(self.myMap.overlays)
+                // remove an existing route line and add  a new one
+                self.myMap.removeOverlays(self.myMap.overlays)
             
-            self.myMap.add((myRoute?.polyline)!, level: MKOverlayLevel.aboveRoads)
-            //scale the map to show the whole route
-            let rect = myRoute?.polyline.boundingMapRect
-            self.myMap.setRegion(MKCoordinateRegionForMapRect(rect!), animated: true)
+                self.myMap.add((myRoute?.polyline)!, level: MKOverlayLevel.aboveRoads)
+                //scale the map to show the whole route
+                let rect = myRoute?.polyline.boundingMapRect
+                self.myMap.setRegion(MKCoordinateRegionForMapRect(rect!), animated: true)
             
-            // get the route steps to show on screen
-             if let steps = myRoute?.steps as [MKRouteStep]!
-             {
-                for step in steps
-                {
-                    showRoute = showRoute + step.instructions
-                } //for ends
-                self.route.text = showRoute
-                } //if ends
+                // get the route steps to show on screen
+                self.steps = myRoute?.steps
+                self.directionsTableView.reloadData()
             }
         
         })
-    
-    
+        
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return steps.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // your cell coding
+        let cellIdentifier = "stepCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! DirectionsTableViewCell
+        
+        cell.number.text = "\(indexPath.row + 1)"
+        cell.Steps.text = self.steps[indexPath.row].instructions
+        return cell
+    }
+    
+
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
     let renderer = MKPolylineRenderer(overlay: overlay)
         renderer.strokeColor = (currentTransportType == .automobile) ?
-            UIColor.blue : UIColor.orange
+            UIColor.blue : UIColor.purple
         renderer.lineWidth = 3.0
         return renderer
     }
